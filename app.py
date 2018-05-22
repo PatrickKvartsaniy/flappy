1cae63f96b98fca189893cc2099c9b781f4d8330
Patrick <kvartsaniy@gmail.com>
2018-05-22 14:21:31 +0300

    change some server config

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__,static_url_path='/static')
@@ -7,4 +9,6 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    print(f"Serve on port: {port}")
+    app.run(app,host="0.0.0.0",port=port)

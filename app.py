688d80628d2761f7aaa067ebee586e8020f3c4b4
Patrick <kvartsaniy@gmail.com>
2018-05-22 14:25:07 +0300

    change host config

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def index():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     print(f"Serve on port: {port}")
-    app.run(app,host="0.0.0.0",port=port)
+    app.run(host="0.0.0.0",port=port)
